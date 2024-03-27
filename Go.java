632c0c761820fe15cc26a3e024322a37af062503
toby-simonsen <ts182680@umconnect.umt.edu>
2024-03-27 11:34:55 -0600

    initial commit

---
+++ Go.java
@@ -0,0 +1,71 @@
+package com.example.project;
+
+import java.util.Scanner;
+
+public class Go {
+
+    static String[][] goBoard = new String[9][9];
+
+    public static void main(String[] args) {
+
+        Boolean cont = true, turn = true;
+        int moveX, moveY;
+
+        Scanner scn = new Scanner(System.in);
+
+        int dimension = goBoard.length - 1;
+
+        while (cont) {
+
+            System.out.println("  0 1 2 3 4 5 6 7 8");
+            for (int i = 0; i < goBoard.length; i++) {
+                System.out.print(i + " ");
+                for (int j = 0; j < goBoard[i].length; j++) {
+                    if (goBoard[i][j] == null) {
+                        if (j == 0)
+                            System.out.print("|");
+                        else
+                            System.out.print("-|");
+
+                    } else {
+                        System.out.print(goBoard[i][j]);
+                    }
+
+                }
+                System.out.println();
+            }
+
+            System.out.println("\n" + ((turn) ? "Black" : "White") + "'s turn to move!\n\n");
+
+            System.out.println("Please enter the x coordinate where you'd like to place a "
+                    + ((turn) ? "Black" : "White") + " piece");
+
+            moveX = scn.nextInt();
+
+            System.out.println("Please enter the y coordinate where you'd like to place a "
+                    + ((turn) ? "Black" : "White") + " piece");
+
+            moveY = scn.nextInt();
+
+            if ((moveY > dimension || moveY < 0) || (moveX > dimension || moveX < 0)) {
+                System.out.println(
+                        "\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\nOut of Bounds!\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+                continue;
+            } else {
+
+                if (goBoard[moveY][moveX] == null) {
+                    goBoard[moveY][moveX] = ((turn) ? "-●" : "-◯");
+                } else {
+                    System.out.println(
+                            "\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\nThere is already a piece there!\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
+                    continue;
+                }
+
+            }
+
+            turn = !turn;
+        }
+
+    }
+
+}
